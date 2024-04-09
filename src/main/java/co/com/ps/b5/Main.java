031493aae9ae1a0e6164a57c85d8bd811e806837
Juan Carlos Narvaez <jothan2107@gmail.com>
2024-04-08 20:30:03 -0500

    Trabajoenclase No3 Trabajo con listas

---
+++ src/main/java/co/com/ps/b5/Main.java
@@ -0,0 +1,15 @@
+package co.com.ps.b5;
+
+public class Main {
+    public static void main(String[] args) {
+        TrabajoEnClase4 Producto = TrabajoEnClase4.ALIMENTOS;
+        System.out.println(Producto);
+        Producto = TrabajoEnClase4.ASEO;
+        System.out.println(Producto);
+        Producto = TrabajoEnClase4.ASEO_PERSONAL;
+        System.out.println(Producto);
+        Producto = TrabajoEnClase4.MASCOTAS;
+        System.out.println(Producto);
+
+    }
+}
---
+++ src/main/java/co/com/ps/b5/TrabajoEnClase4.java
@@ -0,0 +1,6 @@
+package co.com.ps.b5;
+
+public enum TrabajoEnClase4 {
+    //Productos
+    ASEO, ALIMENTOS, ASEO_PERSONAL, MASCOTAS
+}
